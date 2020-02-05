74f2f594098502e0de7caabe3175f8f460d28461
刘大小 <33533223+growvv@users.noreply.github.com>
2020-02-05 22:11:10 +0800

    Add files via upload

---
+++ he-standard-common2.js
@@ -0,0 +1,10 @@
+(function (d) {
+  var c = d.createElement('link')
+  c.rel = 'stylesheet'
+  c.href = 'https://cdn.jsdelivr.net/gh/growvv/CDN/he-standard2.css'
+  var s = d.createElement('script')
+  s.src = 'https://cdn.jsdelivr.net/gh/growvv/CDN/he-standard2.js'
+  var sn = d.getElementsByTagName('script')[0]
+  sn.parentNode.insertBefore(c, sn)
+  sn.parentNode.insertBefore(s, sn)
+})(document)
