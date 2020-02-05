06189ecb8852397f4919f45855ba01f4be4b6c31
刘大小 <33533223+growvv@users.noreply.github.com>
2020-02-05 22:17:51 +0800

    Update he-standard-common2.js

--- he-standard-common2.js
+++ he-standard-common2.js
@@ -1,10 +1,10 @@
 (function (d) {
-  var c = d.createElement('link')
+  var c = d.createElement('link2')
   c.rel = 'stylesheet'
   c.href = 'https://cdn.jsdelivr.net/gh/growvv/CDN/he-standard2.css'
-  var s = d.createElement('script')
+  var s = d.createElement('script2')
   s.src = 'https://cdn.jsdelivr.net/gh/growvv/CDN/he-standard2.js'
-  var sn = d.getElementsByTagName('script')[0]
+  var sn = d.getElementsByTagName('script2')[0]
   sn.parentNode.insertBefore(c, sn)
   sn.parentNode.insertBefore(s, sn)
 })(document)
